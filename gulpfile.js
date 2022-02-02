62c37e1ca7d1dcb52f47b6e1c4f311bbd8420c9b
Akkun <akkunlab@gmail.com>
2022-02-02 19:37:31 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const htmlbeautify = require('gulp-html-beautify')
 const rename = require('gulp-rename');
 const ejs = require('gulp-ejs');
 const replace = require('gulp-replace');
+
+const PATH = 'src/**/';
  
 gulp.task('ejs', done => {
   gulp
-    .src(['ejs/**/*.ejs', '!ejs/**/_*.ejs'])
+    .src([`${PATH}*.ejs`, `!${PATH}_*.ejs`])
     .pipe(ejs())
     .pipe(rename({ extname: '.html' }))
     .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, '$1'))
@@ -14,11 +16,11 @@ gulp.task('ejs', done => {
       'indent_size': 2,
       "end_with_newline": true
     }))
-    .pipe(gulp.dest('./html/'));
+    .pipe(gulp.dest('./docs/'));
   done();
 });
  
 gulp.task('watch', () => {
-  gulp.watch('ejs/**/_*.ejs', gulp.series('ejs'));
-  gulp.watch('ejs/**/*.ejs', gulp.series('ejs'));
+  gulp.watch(`${PATH}_*.ejs`, gulp.series('ejs'));
+  gulp.watch(`${PATH}*.ejs`, gulp.series('ejs'));
 });
