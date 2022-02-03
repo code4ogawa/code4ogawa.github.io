4b9042aa616ace0e9f9217bb1ed9715ba80a6166
Akkun <akkunlab@gmail.com>
2022-02-04 00:15:56 +0900

    Add sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const htmlbeautify = require('gulp-html-beautify')
 const rename = require('gulp-rename');
 const ejs = require('gulp-ejs');
 const replace = require('gulp-replace');
+const sass = require('gulp-sass')(require('sass'));
 
 const PATH = 'src/**/';
  
+/* EJS */
 gulp.task('ejs', done => {
   gulp
     .src([`${PATH}*.ejs`, `!${PATH}_*.ejs`])
@@ -14,13 +16,24 @@ gulp.task('ejs', done => {
     .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, '$1'))
     .pipe(htmlbeautify({
       'indent_size': 2,
-      "end_with_newline": true
+      'end_with_newline': true
     }))
     .pipe(gulp.dest('./docs/'));
   done();
 });
- 
+
+/* Sass */
+gulp.task('sass', done => {
+  gulp
+    .src(`${PATH}*.scss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+  done();
+});
+
+/* Watch */
 gulp.task('watch', () => {
   gulp.watch(`${PATH}_*.ejs`, gulp.series('ejs'));
   gulp.watch(`${PATH}*.ejs`, gulp.series('ejs'));
+  gulp.watch(`${PATH}*.scss`, gulp.series('sass'));
 });
